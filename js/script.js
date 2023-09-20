3d86c9223a37a0324ec49e1bc928065799e940b8
josejunior <jredit7@gmail.com>
2023-09-19 21:02:16 -0300

    Implementa responsividade

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function funOpenMenuMob() {
+    document.getElementById("menumob").style.width = "100%";
+}
+function funCloseMenuMob() {
+    document.getElementById("menumob").style.width = '0%';
+}
