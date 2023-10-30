136d4eb47ee97e093b72ff4ada725dbb4ae4bca7
Jose Junior <jredit7@gmail.com>
2023-10-30 16:44:17 +0000

    Versão 2

--- js/script.js
+++
@@ -1,6 +0,0 @@
-function funOpenMenuMob() {
-    document.getElementById("menumob").style.width = "100%";
-}
-function funCloseMenuMob() {
-    document.getElementById("menumob").style.width = '0%';
-}
