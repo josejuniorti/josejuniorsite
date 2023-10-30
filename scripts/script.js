31a7b616cff0f3a8b47afd065a7ff055c038913a
Jose Junior <jredit7@gmail.com>
2023-10-30 18:29:10 +0000

    Aplica melhorias ao design

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,14 @@
 function launchPro() {
     document.getElementById("pro").style.display = "block";
     document.getElementById("vid").style.display = "none";
-    document.getElementById("probtn").style.backgroundColor = "rgb(2, 71, 199)";
+    document.getElementById("probtn").style.backgroundColor = "rgb(3, 56, 156)";
     document.getElementById("vidbtn").style.backgroundColor = "rgb(0, 89, 255)";
     document.getElementById("againl").style.display = "inline";
 }
 function launchVid() {
     document.getElementById("vid").style.display = "block";
     document.getElementById("pro").style.display = "none";
-    document.getElementById("vidbtn").style.backgroundColor = "rgb(2, 71, 199)";
+    document.getElementById("vidbtn").style.backgroundColor = "rgb(3, 56, 156)";
     document.getElementById("probtn").style.backgroundColor = "rgb(0, 89, 255)";
     document.getElementById("againl").style.display = "inline"; 
 }
